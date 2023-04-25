3dcdb2fbc74bfd6894e0adcbeee343a33098401f
Simone Tomasella <simo64.tomasella@gmail.com>
2023-04-25 19:22:35 +0200

    Added function for checking file extension

--- func.cpp
+++ func.cpp
@@ -10,4 +10,9 @@ void checkStartArgs(int argc, char *args[]){
     struct stat sb;
     if(stat(args[1], &sb) != 0) 
         throw std::invalid_argument("File not found!");
+
+    // Checks if file extension is correct (only .txt files are suppported for now)
+    std::string fileName = args[1];
+    if(fileName.substr(fileName.find_last_of(".")) != ".txt")
+        throw std::invalid_argument("File extension not supported!");
 }
